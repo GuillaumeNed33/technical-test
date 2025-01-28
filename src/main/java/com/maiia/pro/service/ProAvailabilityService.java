5599fac13628326c2b0ce8a0c78655baa4ab8682
Guillaume Nedelec <guillaume.ned33@gmail.com>
2025-01-29 00:46:59 +0100

    Implements availability service to generate availabilities of practitioners

--- src/main/java/com/maiia/pro/service/ProAvailabilityService.java
+++ src/main/java/com/maiia/pro/service/ProAvailabilityService.java
@@ -1,12 +1,16 @@
 package com.maiia.pro.service;
 
+import com.maiia.pro.entity.Appointment;
 import com.maiia.pro.entity.Availability;
+import com.maiia.pro.entity.TimeSlot;
 import com.maiia.pro.repository.AppointmentRepository;
 import com.maiia.pro.repository.AvailabilityRepository;
 import com.maiia.pro.repository.TimeSlotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +31,107 @@ public class ProAvailabilityService {
     }
 
     public List<Availability> generateAvailabilities(Integer practitionerId) {
-        // TODO : implement this
-        return new ArrayList<>();
+        if(practitionerId != null) {
+            List<TimeSlot> timeslots = timeSlotRepository.findByPractitionerId(practitionerId);
+            List<Availability> existingAvailabilities = this.availabilityRepository.findByPractitionerId(practitionerId);
+            List<Appointment> appointments = this.appointmentRepository.findByPractitionerId(practitionerId);
+            List<Availability> availabilitiesToAdd = new ArrayList<>();
+
+            for(TimeSlot timeSlot : timeslots) {
+                LocalDateTime startDateNewAvailability = timeSlot.getStartDate();
+                LocalDateTime endDateNewAvailability = startDateNewAvailability.plusMinutes(15);
+
+                // Check if new availability not exceeding the slot endDate
+                while (timeSlot.getEndDate().isAfter(endDateNewAvailability) ||
+                        timeSlot.getEndDate().isEqual(endDateNewAvailability)) {
+
+                    Availability existingAvailability = hasOverlapAvailability(existingAvailabilities, startDateNewAvailability, endDateNewAvailability);
+                    Appointment existingAppointment = hasAppointmentInThisSlot(appointments, startDateNewAvailability, endDateNewAvailability);
+
+                    if(existingAvailability == null && existingAppointment == null) {
+                        availabilitiesToAdd.add(
+                                new Availability(
+                                        null,
+                                        practitionerId,
+                                        startDateNewAvailability,
+                                        endDateNewAvailability
+                                )
+                        );
+                        startDateNewAvailability = startDateNewAvailability.plusMinutes(15);
+                    }
+
+                    //If existing availability/appointment on current time -> start new availability at the end of existing availability/appointment
+                    else {
+                        LocalDateTime newStart;
+                        if(existingAvailability != null && existingAppointment != null) {
+                            newStart = existingAvailability.getEndDate().isAfter(existingAppointment.getEndDate())
+                                    ? existingAvailability.getEndDate() : existingAppointment.getEndDate();
+                        } else {
+                            newStart = existingAvailability != null ? existingAvailability.getEndDate() : existingAppointment.getEndDate();
+                        }
+                        startDateNewAvailability = newStart;
+                    }
+
+                    //Try to generate 15min long new availability
+                    long minutesBeforeEnd = Duration.between(startDateNewAvailability, timeSlot.getEndDate()).toMinutes();
+                    if(minutesBeforeEnd >= 15) {
+                        endDateNewAvailability = startDateNewAvailability.plusMinutes(15);
+                    }
+
+                    // if not possible, generate shorter one
+                    else if(minutesBeforeEnd > 0) {
+                        endDateNewAvailability = startDateNewAvailability.plusMinutes(minutesBeforeEnd);
+                    }
+
+                    //else : add value to close while loop
+                    else {
+                        endDateNewAvailability = startDateNewAvailability.plusMinutes(15);
+                    }
+                }
+            }
+
+            availabilityRepository.saveAll(availabilitiesToAdd);
+            return availabilitiesToAdd;
+        }
+        throw new IllegalArgumentException("practitionerId is null");
+    }
+
+
+    // TODO optimisation : refactoriser hasAppointmentInThisSlot et hasOverlapAvailability
+    // meme code pour 2 types d'objets
+    private Appointment hasAppointmentInThisSlot(List<Appointment> appointments,
+                                             LocalDateTime startDateNewAvailability,
+                                             LocalDateTime endDateNewAvailability) {
+
+        if(startDateNewAvailability.isAfter(endDateNewAvailability)) {
+            throw new IllegalArgumentException("startDate is after endDate");
+        }
+
+        for(Appointment appointment : appointments) {
+            if (!(!appointment.getEndDate().isAfter(startDateNewAvailability) ||
+                    !appointment.getStartDate().isBefore(endDateNewAvailability))) {
+                return appointment;
+            }
+        }
+        return null;
+    }
+
+
+    private Availability hasOverlapAvailability(
+            List<Availability> existingAvailabilities,
+            LocalDateTime startDateNewAvailability,
+            LocalDateTime endDateNewAvailability) {
+
+        if(startDateNewAvailability.isAfter(endDateNewAvailability)) {
+            throw new IllegalArgumentException("startDate is after endDate");
+        }
+
+        for(Availability availability : existingAvailabilities) {
+            if (!(!availability.getEndDate().isAfter(startDateNewAvailability) ||
+                   !availability.getStartDate().isBefore(endDateNewAvailability))) {
+                return availability;
+            }
+        }
+        return null;
     }
 }
